c42c4d75d5a87c89bc48d6dfbe127ef87afd8892
Thr1ve <fict1on.buhler@gmail.com>
2016-07-17 15:03:40 -0400

    make flex-direction in Card component dynamic

--- app/components/Card/index.js
+++ app/components/Card/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import styles from './styles.css';
 
-const Card = ({ children, }) =>
-  <div className={styles.card}>
+const Card = ({ children, column = false }) =>
+  <div
+    className={styles.card}
+    style={{ flexDirection: `${column ? 'column' : 'row'}` }}
+  >
     {children}
   </div>;
+
+Card.propTypes = {
+  column: PropTypes.bool,
+  children: PropTypes.array
+};
 
 export default Card;
