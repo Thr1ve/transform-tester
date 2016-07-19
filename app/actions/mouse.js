248bcfabc5612540fd379bc6d31529da8eb4ca2b
Thr1ve <fict1on.buhler@gmail.com>
2016-07-19 19:32:00 -0400

    disable transition before mousemove/scroll

--- app/actions/mouse.js
+++ app/actions/mouse.js
@@ -1,11 +1,13 @@
-import { updateMoveBoxState } from '../actions';
+import { updateMoveBoxState, disableTransition } from '../actions';
 
 export const mouseMove = ({ x, y }) => (dispatch, getState) => {
   const { selected, moveBox } = getState().transformTester;
   if (selected === 'TRANSLATE') {
+    dispatch(disableTransition());
     dispatch(updateMoveBoxState('tx', moveBox.tx + x));
     dispatch(updateMoveBoxState('ty', moveBox.ty - y));
   } else if (selected === 'ROTATE') {
+    dispatch(disableTransition());
     dispatch(updateMoveBoxState('rx', moveBox.rx - y));
     dispatch(updateMoveBoxState('ry', moveBox.ry + x));
   }
@@ -14,8 +16,10 @@ export const mouseMove = ({ x, y }) => (dispatch, getState) => {
 export const scroll = ({ z }) => (dispatch, getState) => {
   const { selected, moveBox } = getState().transformTester;
   if (selected === 'TRANSLATE') {
+    dispatch(disableTransition());
     dispatch(updateMoveBoxState('tz', moveBox.tz + z));
   } else if (selected === 'ROTATE') {
+    dispatch(disableTransition());
     dispatch(updateMoveBoxState('rz', moveBox.rz - z));
   }
 };
